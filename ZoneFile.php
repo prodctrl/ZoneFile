294c68fa8aa36310a474c11e32796a18812e4534
Addison Zimmerlin <addisonzimmerlin@gmail.com>
2020-04-23 19:30:48 -0700

    Progress commit

--- ZoneFile.php
+++ ZoneFile.php
@@ -16,6 +16,48 @@ class ZoneFile {
 		if( !is_null($domain) ) $this->domain = $domain;
 		if( !is_null($ttl) ) $this->ttl = $ttl;
 	}
+
+
+	private function addRecord($name, $ttl, $class, $type, $data){
+		$this->records[] = [
+			'name' => $name,
+			'ttl' => $ttl,
+			'class' => $class,
+			'type' => $type,
+			'data' => $data
+		];
+	}
+
+
+	private function calculateStrlenMaxes(){
+		foreach($this->records as $record){
+			foreach(['name', 'ttl', 'class', 'type'] as $key){
+				$len = strlen($record[$key]);
+
+				if( $len>$this->strlen_maxes[$key] ){
+					$this->strlen_maxes[$key] = $len;
+				}
+			}
+		}
+	}
+
+
+	private function pad($input, $max_len){
+		$pad = 1; //The number of extra tabs padding
+		$chars_per_tab = 8;
+
+		$max_chars = (
+			ceil($max_len/$chars_per_tab) + $pad
+		) * $chars_per_tab;
+
+		$tabs_needed = ceil(
+			(
+				$max_chars-strlen($input)
+			)/$chars_per_tab
+		);
+
+		return $input . str_repeat("\t", $tabs_needed);
+	}
 }
 
 ?>
