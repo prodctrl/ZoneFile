fd8c120466d0b7c8b341bee68ad01d48d0225ad2
Addison Zimmerlin <addisonzimmerlin@gmail.com>
2020-05-05 16:18:56 -0700

    Progress commit

--- generate-zone-file.sh
+++ generate-zone-file.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 
+# Usage:
+	# sh generate-zone-file.sh domain_name github_username github_repo
+
+# Example:
+	# sh generate-zone-file.sh productioncontrol.tv prodctrl dns
+
+# Note:
+	# Do not include a period at the end of the domain name
+
+
+# Command-line styles
+style_reset="\e[0m"
+style_success="\e[92m"
+style_error="\e[1;91m"
+style_advisory="\e[93m"
+style_special="\e[1;94m"
