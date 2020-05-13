926b91fc9bc1ed1009e4b6ba33de19a495011a4c
Addison Zimmerlin <addisonzimmerlin@gmail.com>
2020-05-13 08:58:06 -0700

    Send domain name and repo as $argv[1-2]

--- generate-zone-file.sh
+++ generate-zone-file.sh
@@ -79,7 +79,7 @@ sleep 1
 # Generate zone file
 echo
 echo "${style_advisory}Generating zone file...${style_reset}"
-php $dir_github_repo/zone-file-generator.php "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "$12" "$13" "$14" "$15" "$16" "$17" "$18" "$19" "$20" "$21" "$22" "$23" "$24" "$25" "$26" "$27" "$28" "$29" "$30" "$31" "$32" "$33" "$34" > $zone_file_output
+php $dir_github_repo/zone-file-generator.php "$domain" "$github_repo" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "$12" "$13" "$14" "$15" "$16" "$17" "$18" "$19" "$20" "$21" "$22" "$23" "$24" "$25" "$26" "$27" "$28" "$29" "$30" "$31" "$32" "$33" "$34" > $zone_file_output
 sleep 1
 
 echo
